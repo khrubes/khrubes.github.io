b061e422927871ffa5a4c86fa5cccc1f37973be6
Kelsey Hrubes <kelseyhrubes@gmail.com>
2015-08-09 16:39:25 -0700

    okay I'm done for today

--- js/init.js
+++ js/init.js
@@ -1,48 +1,30 @@
-$( document ).ready(function() {
-    animateThingsIAm();
-});
-
-// var isMobile = {
-//     Android: function() {
-//         return navigator.userAgent.match(/Android/i);
-//     },
-//     BlackBerry: function() {
-//         return navigator.userAgent.match(/BlackBerry/i);
-//     },
-//     iOS: function() {
-//         return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-//     },
-//     Opera: function() {
-//         return navigator.userAgent.match(/Opera Mini/i);
-//     },
-//     Windows: function() {
-//         return navigator.userAgent.match(/IEMobile/i);
-//     },
-//     any: function() {
-//         return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-//     }
-// };
+var animateName = function() {
+    var name = $( ".name" );
+    name.fadeIn(2000);
+};
 
 var thingsIAm = [
-"a web developer",
-"a computer science student",
-"from Iowa",
-"a hackathon organizer",
-"a German speaker",
-"a coffee snob",
+"web developer",
+"computer science student",
+"hackathon organizer",
+"coffee snob",
 ];
 
 var thingsIndex = 0;
 
 var animateThingsIAm = function () {
     thingsIndex = (thingsIndex >= thingsIAm.length) ? 0 : thingsIndex;
-    $( ".thingIAmText" ).fadeIn("slow", function(){
-        $( ".thingIAmText" ).delay( 1000 );
-        $( ".thingIAmText" ).fadeOut("slow", function(){
-            $( ".thingIAmText" ).html( thingsIAm[thingsIndex++] );
+    var text = $( ".thingIAmText" );
+    text.fadeIn(thingsIndex == 0 ? 600 : 1000, function(){
+        text.fadeOut(1000, function(){
+            text.html( thingsIAm[thingsIndex++] );
             animateThingsIAm();
         });
     });   
 };
 
 
+$( document ).ready(function() {
+    animateName();
+    animateThingsIAm();
+});
