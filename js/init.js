4c83ab69d96a4fef35df36f2a2be29391ea372d6
Kelsey Hrubes <kelseyhrubes@gmail.com>
2015-08-09 18:16:05 -0700

    okay NOW I'm done

--- js/init.js
+++ js/init.js
@@ -1,30 +1,37 @@
-var animateName = function() {
-    var name = $( ".name" );
-    name.fadeIn(2000);
-};
+// var animateName = function() {
+//     var name = $( ".name" );
+//     name.fadeIn(2000);
+// };
 
-var thingsIAm = [
-"web developer",
-"computer science student",
-"hackathon organizer",
-"coffee snob",
-];
+// var thingsIAm = [
+// "web developer",
+// "computer science student",
+// "hackathon organizer",
+// "coffee snob",
+// ];
 
-var thingsIndex = 0;
+// var thingsIndex = 0;
 
-var animateThingsIAm = function () {
-    thingsIndex = (thingsIndex >= thingsIAm.length) ? 0 : thingsIndex;
-    var text = $( ".thingIAmText" );
-    text.fadeIn(thingsIndex == 0 ? 600 : 1000, function(){
-        text.fadeOut(1000, function(){
-            text.html( thingsIAm[thingsIndex++] );
-            animateThingsIAm();
-        });
-    });   
-};
+// var animateThingsIAm = function () {
+//     thingsIndex = getThingsIndex();
+//     var text = $( ".thingIAmText" );
+//     text.fadeIn(1000, function(){
+//         text.fadeOut(1000, function(){
+//             text.html( thingsIAm[thingsIndex++] );
+//             animateThingsIAm();
+//         });
+//     });   
+// };
+
+// var getThingsIndex = function(){
+//     if(thingsIndex >= thingsIAm.length) {
+//         thingsIndex = 0;
+//     }
+//     return thingsIndex;
+// }
 
 
-$( document ).ready(function() {
-    animateName();
-    animateThingsIAm();
-});
+// $( document ).ready(function() {
+//     animateName();
+//     animateThingsIAm();
+// });
