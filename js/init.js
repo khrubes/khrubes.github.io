8c708c60c7c21a81f21af9f965e54a8d9c41e514
Kelsey Hrubes <kelseyhrubes@gmail.com>
2015-08-16 19:46:20 -0500

    add scrollfire/staggered list to experience section

--- js/init.js
+++ js/init.js
@@ -1,9 +1,31 @@
+ var isMobile = function() {
+    return window.innerWidth <= 720;
+};
+
+var initScrollFire = function() {
+    debugger;
+    var options = [
+        {selector: '.experience-ul', offset: 100, callback: 'Materialize.showStaggeredList(".experience-ul")'},
+    ];
+    Materialize.scrollFire(options);
+};
+
+
 /** Once the DOM is ready. */
 $( document ).ready(function(){
     $(".button-collapse").sideNav();
+    if (!isMobile()) {
+        $('.parallax').parallax();
+    } else {
+        $('.parallax-container').removeClass('parallax-container');
+        $('.parallax').removeClass('parallax');
+    }
 });
+
 
 /** Once everything, including images, is ready */
 $( window ).load(function() {
     $(".splash-text").fadeIn(1500);
+    initScrollFire();
 });
+
