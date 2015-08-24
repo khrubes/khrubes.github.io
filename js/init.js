e867e098073178ab6a97ed6b46621ce6f7c1c26a
Kelsey Hrubes <kelseyhrubes@gmail.com>
2015-08-24 10:36:17 -0500

    fix text opacity issue!!

--- js/init.js
+++ js/init.js
@@ -24,7 +24,6 @@ $( document ).ready(function(){
 
 /** Once everything, including images, is ready */
 $( window ).load(function() {
-    $(".splash-text").fadeIn(1500);
     var scrollfireManager = new ScrollFireManager();
     scrollfireManager.initScrollFire();
 });
